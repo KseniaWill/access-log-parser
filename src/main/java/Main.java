f1246a08e6b286f724931b1a670cbe40e9e8375a
Ksenia Vill <git.user.ksen.ka_v@mail.ru>
2025-09-13 16:31:53 +0300

    Операции с числами

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: "+text.length());
+        System.out.println("Введите число:");
+        int num_1 = new Scanner(System.in).nextInt();
+        System.out.println("Введите число:");
+        int num_2 = new Scanner(System.in).nextInt();
+        int sum = num_1 + num_2;
+        System.out.println("Сумма введенных чисел:" + sum);
+        int diff = num_1 - num_2;
+        System.out.println("Разность введенных чисел:" + diff);
+        int product = num_1 * num_2;
+        System.out.println("Произведение введенных чисел:" + product);
+        double div = (double) num_1 / num_2;
+        System.out.println("Частное введенных чисел:" + div);
 
     }
 }
